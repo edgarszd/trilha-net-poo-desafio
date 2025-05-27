073526acd7626d45bd9900859bac8c04bd99c9ed
edgarszd <edgarsz@academico.ufs.br>
2025-05-27 20:05:12 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("999999", "Tijolão", "1111111", 2);
+
+n1.Ligar();
+n1.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Iphone i1 = new Iphone("888888", "Iphone 1", "2222222", 1);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
